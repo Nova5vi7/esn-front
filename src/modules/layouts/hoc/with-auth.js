b669c1d6186ceaae562acbb4c0aa4b572c419918
Nova5vi7 <anatolijk096@gmail.com>
2021-07-22 21:29:17 +0300

    fix display name

--- src/modules/layouts/hoc/with-auth.js
+++ src/modules/layouts/hoc/with-auth.js
@@ -2,35 +2,33 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import verifyToken from '../../../services/auth/verify-token';
 
-const WithAuth = WrappedComponent =>
-  properties => {
-    const Router = useRouter();
-    const [verified, setVerified] = useState(false);
+const WithAuth = WrappedComponent => properties => {
+  const Router = useRouter();
+  const [verified, setVerified] = useState(false);
 
-    useEffect(() => {
-      const verifyLogic = async () => {
-        const accessToken = localStorage.getItem('token');
+  useEffect(() => {
+    const verifyLogic = async () => {
+      const accessToken = localStorage.getItem('token');
 
-        if (!accessToken) {
-          Router.replace('/sign-in');
+      if (!accessToken) {
+        Router.replace('/sign-in');
+      } else {
+        const data = await verifyToken();
+
+        if (data) {
+          setVerified(true);
         } else {
-          const data = await verifyToken();
+          localStorage.removeItem('token');
 
-          if (data) {
-            setVerified(true);
-          } else {
-            localStorage.removeItem('token');
-
-            Router.replace('/sign-in');
-          }
+          Router.replace('/sign-in');
         }
-      };
+      }
+    };
 
-      verifyLogic();
-    }, [Router]);
+    verifyLogic();
+  }, [Router]);
 
-    return <>{verified ? <WrappedComponent {...properties} /> : null}</>;
-  };
-
+  return <>{verified ? <WrappedComponent {...properties} /> : null}</>;
+};
 
 export default WithAuth;
