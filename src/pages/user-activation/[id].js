25055bebe2527c7e8d99b6eb73f49fedc274a0a9
Nova5vi7 <anatolijk096@gmail.com>
2021-07-22 22:52:21 +0300

    fix user activate files

--- src/pages/user-activation/[id].js
+++ src/pages/user-activation/[id].js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import Wrap from '@/modules/layouts/main';
-import UserActivate from "../../modules/user-activation";
+import UserActivate from '../../modules/user-activation';
 
 const UserActivatePage = () => {
   return (
     <Wrap>
-      <UserActivate/>
+      <UserActivate />
     </Wrap>
   );
-}
+};
 
 export default UserActivatePage;
