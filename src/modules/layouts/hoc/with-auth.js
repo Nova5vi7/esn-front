9163bcaadf2ee1e7e4bf90439516fe12fadc314b
Nova5vi7 <anatolijk096@gmail.com>
2021-07-24 02:35:36 +0300

    added fix from comments

--- src/modules/layouts/hoc/with-auth.js
+++ src/modules/layouts/hoc/with-auth.js
@@ -1,34 +1,41 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import verifyToken from 'services/auth/verify-token';
+import LoadingComponent from '@/components/loader';
 
 const WithAuth = WrappedComponent => properties => {
-  const Router = useRouter();
+  const router = useRouter();
   const [verified, setVerified] = useState(false);
+  const [loadingStatus, setLoadingStatus] = useState(true);
 
   const verifyLogic = useCallback(async () => {
     const accessToken = localStorage.getItem('token');
 
     if (!accessToken) {
-      Router.replace('/sign-in');
+      router.replace('/sign-in');
     } else {
-      const data = await verifyToken();
+      const response = await verifyToken();
 
-      if (data) {
+      if (response) {
         setVerified(true);
       } else {
         localStorage.removeItem('token');
 
-        Router.replace('/sign-in');
+        router.replace('/sign-in');
       }
     }
-  }, [Router]);
+    setLoadingStatus(false);
+  }, [router]);
 
   useEffect(() => {
     verifyLogic();
   }, [verifyLogic]);
 
-  return <>{verified && <WrappedComponent {...properties} />}</>;
+  if (loadingStatus) {
+    return <LoadingComponent />;
+  } else if (verified) {
+    return <WrappedComponent {...properties} />;
+  }
 };
 
 export default WithAuth;
