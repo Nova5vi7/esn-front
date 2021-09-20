ff6b4f75050c10ddaeccfae8091aa9b9f5c17373
ab <ytkonaft@gmail.com>
2021-09-21 00:39:35 +0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     node: true,
     es6: true
   },
-  plugins: ['simple-import-sort', 'react-hooks'],
+  plugins: ['simple-import-sort'],
   settings: {
     react: {
       version: 'detect'
@@ -23,31 +23,15 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:prettier/recommended',
     'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
     'plugin:security/recommended',
     'plugin:react-hooks/recommended'
   ],
   rules: {
-    'no-case-declarations': 'off',
-    'no-console': 'off',
-    'react/prop-types': 'off',
-    'simple-import-sort/imports': 'off',
-    'simple-import-sort/exports': 'off',
-    'sonarjs/no-small-switch': 'off',
-    'unicorn/no-abusive-eslint-disable': 'off',
-    'react/display-name': 'off',
-    'unicorn/prefer-ternary': 'off',
-    'jsx-a11y/href-no-hash': 0,
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton']
-      }
-    ],
+    'no-console': 'error',
     'react/react-in-jsx-scope': 'off',
-    'unicorn/consistent-function-scoping': 'off',
-    'react-hooks/rules-of-hooks': 'off',
-    'unicorn/no-null': 'off'
+    'react/prop-types': 'off',
+    'simple-import-sort/sort': 'error',
+    'unicorn/filename-case': 'off'
   }
 };
